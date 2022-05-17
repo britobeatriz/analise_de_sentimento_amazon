e624ed4009fc46b63e1abaa568bfa1d8a377c9a7
Beatriz <beatriz.debritolima1998@gmail.com>
2022-05-17 14:22:39 -0300

    dados completos

--- scraping.py
+++ scraping.py
@@ -5,7 +5,7 @@ import pandas as pd
 dados = []
 
 for i in range(45):
-    url ='https://www.amazon.com.br/Como-fazer-amigos-influenciar-pessoas/product-reviews/8543108683/ref=cm_cr_arp_d_paging_btm_prev_44'+str(i+1)+'&__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&qid=1633879188&ref=sr_pg_1'
+    url ='https://www.amazon.com.br/Como-fazer-amigos-influenciar-pessoas/product-reviews/8543108683/ref=cm_cr_getr_d_paging_btm_prev_'+str(i+1)+'?pageNumber='+str(i+1)+''
     headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36'}
     req = Request(url, headers = headers)
     response = urlopen(req)
